449fbdf4b8c70abe2236d1cf540ad4541d83fbf3
Enzo Falone <enzo@Enzos-MacBook-Air.local>
2023-02-20 21:46:49 -0500

    Completed lab

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Calendar from './Calendar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itinerary for 7 Days in Chicago 🕺</h1>
+      <h2>Welcome to Chicago, Enzo! Check out this calendar to get to know the city and see all the sights during your stay.</h2>
+    <Calendar/>
+    </div>
+  )
+}
+export default App
